8bce22032b0a41e96e8a8859b59b2edb7f6dad34
Zakariae-Assabiri <assabiri.zakariae@gmail.com>
2025-03-15 01:39:23 +0100

    Décomentation AJouter

--- src/main/java/Car/project/Repositories/ExpenseRepository.java
+++ src/main/java/Car/project/Repositories/ExpenseRepository.java
@@ -4,11 +4,15 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import Car.project.Entities.Expense;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 	    List<Expense> findByDateBetween(LocalDateTime startDate, LocalDateTime endDate);
-	    Double sumMontantByDateBetween(LocalDateTime startDate, LocalDateTime endDate);
+	    @Query("SELECT SUM(e.montant) FROM Expense e WHERE e.date BETWEEN :startDate AND :endDate")
+	    Double sumMontantByDateBetween(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
+
 	}
 
